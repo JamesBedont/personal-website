e08d836d42c966dcd6451f3f76d2ee6103aa1593
James Bedont <jamesbedont@gmail.com>
2020-01-23 20:23:59 -0500

    better title area

--- components/header.js
+++ components/header.js
@@ -1,9 +1,13 @@
 const Header = () => {
   return (
     <header>
-      <img src='/headshot.jpeg' id='headshot' />
-      <h1 id='name'>James Bedont</h1>
-      <h2 id='title'>Software Engineer</h2>
+      <div id='title-wrapper'>
+        <img src='/headshot.jpeg' id='headshot' />
+        <div id='title-text'>
+          <h1 id='name'>James Bedont</h1>
+          <h2 id='title'>Software Engineer</h2>
+        </div>
+      </div>
       <nav>
         <ul>
           <li>Writing</li>
