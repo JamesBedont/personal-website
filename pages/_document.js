f2c2c58944b9bc2c077a4bbad7480fd9c33a36df
James Bedont <jamesbedont@gmail.com>
2020-01-24 17:42:10 -0500

    added favicon

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='shortcut icon' type='image/x-icon' href='/favicon.png' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
