357610b5a73e8df326c2810d2e74f81104cb1008
James Bedont <jamesbedont@gmail.com>
2020-01-25 14:05:35 -0500

    finalized footer

--- components/footer.js
+++ components/footer.js
@@ -1,22 +1,11 @@
 const Footer = () => {
   return (
     <footer className='footer'>
-      <div className='footer-line'></div>
-      <div className='footer-content'>
-        <ul className='list-inline'>
-          <li>
-            <a href='https://www.github.com/JamesBedont'>Github</a>
-          </li>
-          <li>.</li>
-          <li>
-            <a href='https://www.linkedin.com/in/jamesbedont'>Linkedin</a>
-          </li>
-          <li>.</li>
-          <li>
-            <a href='https://www.twitter.com/jamesbedont'>Twitter</a>
-          </li>
-        </ul>
-      </div>
+      <a href='/'>Github</a>
+      <span className='footer-text-separator'>•</span>
+      <a href='/about'>Linkedin</a>
+      <span className='footer-text-separator'>•</span>
+      <a href='/archive'>Twitter</a>
     </footer>
   );
 };
