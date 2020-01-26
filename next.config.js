5333ffb5a926325537188f4e6e08afbd04a9dd38
James Bedont <jamesbedont@gmail.com>
2020-01-26 11:21:03 -0500

    add on to next config by passing slugs into dynamic post page at build time to make room for static exporting

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   webpack: function(config) {
     config.module.rules.push({
@@ -5,5 +8,27 @@ module.exports = {
       use: 'raw-loader'
     });
     return config;
+  },
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    const posts = fs.readdirSync(path.join(__dirname, 'posts'));
+
+    const postConfiguration = posts.reduce((config, postFileName) => {
+      const titleAsFileName = postFileName.replace(/\.md/g, '');
+      return {
+        ...config,
+        [`/${titleAsFileName}`]: {
+          page: '/[slug]',
+          query: { slug: titleAsFileName }
+        }
+      };
+    }, {});
+
+    return {
+      ...postConfiguration,
+      '/': { page: '/' }
+    };
   }
 };
