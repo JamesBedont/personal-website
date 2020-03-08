12c1c4fa47b6818ab1f8a07b6fd93ea38c89baf2
James Bedont <jamesbedont@gmail.com>
2020-03-07 22:56:51 -0500

    log out podcasts in about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,14 @@
 import UnderConstruction from '../components/underConstruction';
 import Layout from '../components/layout';
+import { useEffect } from 'react';
 
 const AboutPage = () => {
+  useEffect(() => {
+    fetch('api/podcasts')
+      .then(res => res.json())
+      .then(podcasts => console.log(podcasts));
+  }, []);
+
   return (
     <Layout title={'About'}>
       <UnderConstruction />
