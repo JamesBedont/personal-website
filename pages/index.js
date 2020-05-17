417fa1f2de4a82371ef2c9edc45191d7ccfdd828
James Bedont <jamesbedont@gmail.com>
2020-05-17 17:56:33 -0400

    create a page service to clean things up

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import matter from 'gray-matter';
 import Post from 'components/post';
 import PropTypes from 'prop-types';
 import Layout from 'components/layout';
-import fs from 'fs';
-import path from 'path';
+import { getAllPosts } from 'lib/pageService';
 
 const HomePage = ({ posts }) => {
   const postElements = posts.map((post, idx) => <Post key={idx} {...post} />);
@@ -19,41 +17,15 @@ HomePage.propTypes = {
     PropTypes.shape({
       title: PropTypes.string.isRequired,
       date: PropTypes.string.isRequired,
-      link: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
     })
   ).isRequired,
 };
 
-export async function getStaticProps() {
-  const posts = fs
-    .readdirSync(path.join(process.cwd(), 'posts'))
-    .map((postFileName) => {
-      const fileContents = fs.readFileSync(
-        path.join(process.cwd(), 'posts', `${postFileName}`),
-        { encoding: 'utf8' }
-      );
-      const { data: frontMatter } = matter(fileContents);
-
-      const date = new Intl.DateTimeFormat('en-US', {
-        month: 'long',
-        day: 'numeric',
-        year: 'numeric',
-      }).format(new Date(frontMatter.date));
-
-      const link = frontMatter.title.toLowerCase().replace(/\s/g, '-');
-
-      return {
-        title: frontMatter.title,
-        link,
-        date,
-      };
-    });
-
-  posts.sort((a, b) => new Date(b.date) - new Date(a.date));
-
+export function getStaticProps() {
   return {
     props: {
-      posts,
+      posts: getAllPosts(),
     },
   };
 }
