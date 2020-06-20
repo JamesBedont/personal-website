ab3ba845bf7826745301306381e6f5dad55e954f
James Bedont <jamesbedont@gmail.com>
2020-06-20 13:02:21 -0400

    use atom instead of rss2 don't use es modules syntax in page service provide a json feed as well rename config file to vercel see if replaceing hostname is required or not

---
+++ buildFeed.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const { Feed } = require('feed');
+const { getAllPosts } = require('./lib/pageService');
+
+const author = { name: 'James Bedont', link: 'https://jamesbedont.com' };
+
+const feed = new Feed({
+  title: "James Bedont's Writing",
+  description: 'Feed of all the articles written on jamesbedont.com',
+  id: 'https://jamesbedont.com/',
+  link: 'https://jamesbedont.com/',
+  language: 'en',
+  image: 'https://jamesbedont.com/headshot.jpeg',
+  favicon: 'https://jamesbedont.com/favicon.png',
+  copyright: `All rights reserved ${new Date().getFullYear()}, James Bedont`,
+  author,
+});
+
+feed.addCategory('Software Development');
+
+getAllPosts().map((post) => {
+  const postUrl = `https://jamesbedont.com/${post.slug}`;
+
+  feed.addItem({
+    title: post.title,
+    description: post.description,
+    link: postUrl,
+    content: post.contentHTML,
+    author: [author],
+    published: new Date(post.date),
+    date: new Date(post.date),
+  });
+});
+
+fs.writeFileSync(path.join(__dirname, 'public', 'feed.json'), feed.json1());
+fs.writeFileSync(path.join(__dirname, 'public', 'feed.xml'), feed.atom1());
