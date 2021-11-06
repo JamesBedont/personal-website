f78216a5b33c41b1510a2be5e57a1bed39205e71
James Bedont <jamesbedont@gmail.com>
2021-11-06 18:53:06 -0400

    remove lambda function and caller

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,7 @@
 import UnderConstruction from 'components/underConstruction';
 import Layout from 'components/layout';
-import { useEffect } from 'react';
 
 const AboutPage = () => {
-  useEffect(() => {
-    fetch('api/podcasts')
-      .then((res) => res.json())
-      .then((podcasts) => console.log(podcasts));
-  }, []);
-
   return (
     <Layout title={'About'} description={'about James Bedont'}>
       <UnderConstruction />
